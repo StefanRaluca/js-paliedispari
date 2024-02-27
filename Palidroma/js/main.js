e9536c9f31bf8ee3f711121b74e5bd4eeb3428ea
StefanRaluca <raluca.bubulina@yahoo.com>
2024-02-27 14:33:04 +0100

    add palindromeWords

--- Palidroma/js/main.js
+++ Palidroma/js/main.js
@@ -1,3 +1,4 @@
+
 //Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
 
 
@@ -7,12 +8,24 @@ let word = prompt('Type a word:');
 //stampiamo nella console
 console.log(word);
 //stabiliamo il modo in quale calcola la parola inserita dal utente 
-let wordReverse = word.split('').reverse().join('');
+//let wordReverse = word.split('').reverse().join('');
 ////stampiamo nella console
-console.log(wordReverse);
-//condizione per controllare la parola
-if (word === wordReverse) {
-    alert('The entered word is a palindrome.');
-} else {
-    alert('The entered word is not a palindrome.');
+//console.log(wordReverse);
+
+//chiamata alla funzione per controllare se la parola è un palindromo
+
+
+//definizione della funzione per controllare se una parola è un palindromo
+
+function palindromeWords(word) {
+    let wordReverse = word.split('').reverse().join('');
+    if (word === wordReverse) {
+        alert('The entered word is a palindrome.');
+    } else {
+        alert('The entered word is not a palindrome.');
+    }
+
+    console.log(wordReverse);
 }
+palindromeWords(word);
+
